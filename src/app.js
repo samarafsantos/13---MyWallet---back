7c635baa0de4bb94395252d670efd4df3519315e
samarafsantos <samarasantos@poli.ufrj.br>
2020-12-04 18:35:07 -0300

    Total value being calculated correctly (all values)

--- src/app.js
+++ src/app.js
@@ -17,7 +17,7 @@ app.post("/api/logout", usersController.logout);
 // Logs routes
 app.get("/api/log", logController.getLog);
 app.post("/api/entry", logController.postEntry);
-// app.post("/api/pullout", authMiddleware, logController.postPullOut);
+app.post("/api/pullout", logController.postPullOut);
 
 const port = 3000;
 
--- src/controllers/logController.js
+++ src/controllers/logController.js
@@ -46,7 +46,30 @@ async function postEntry(req, res){
 
 }
 
+async function postPullOut(req, res){
+    const entries = req.body;
+    const authHeader = req.header('Authorization');
+    const token = authHeader.replace('Bearer ', '');
+    try{
+        const user = await connection.query('SELECT * FROM sessions WHERE token = $1',[token]);
+        try{
+            const time = dayjs().format('DD/MM/YYYY');
+            console.log(time);
+            const logs = await connection.query('INSERT INTO logs ("id_user", date, description, value, type) VALUES ($1, $2, $3, $4, $5) RETURNING *',[user.rows[0].userId, time, entries.description, entries.num, "sub"]);
+            console.log('log',logs.rows);
+            res.status(201).send(logs.rows);
+        }catch(e){
+            console.log(e);
+            res.sendStatus(500);
+        }
+    }catch(e){
+        console.log(e);
+        res.sendStatus(500);
+    }
+}
+
 module.exports = {
     getLog,
-    postEntry
+    postEntry,
+    postPullOut
 };
